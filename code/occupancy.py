a2cef1e2580c87cceae85ce76f5370c690dae5ef
Aadith Warrier <warrieraadith@gmail.com>
2024-09-05 18:36:25 +0530

    occupancy grid connected to input

--- code/occupancy.py
+++ code/occupancy.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python
 
-import rospy
+import rclpy
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
+from PIL import Image
+import time
+from rclpy.node import Node
+import os
 
-def occupancy_grid_callback(msg):
-    # Get grid metadata
-    width = msg.info.width
-    height = msg.info.height
+class MapSubscriber(Node):
+    
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            OccupancyGrid,
+            '/map',
+            self.occupancy_grid_callback,
+            10)
+        self.subscription
 
-    # Convert the 1D data to a 2D numpy array
-    occupancy_grid_2d = np.array(msg.data).reshape((height, width))
+    def occupancy_grid_callback(self, msg):
+        self.get_logger().info("got map")
+        # Get grid metadata
+        width = msg.info.width
+        height = msg.info.height
+        save_dir = os.path.join('code', 'data','INPUT','new')
+        os.makedirs(save_dir, exist_ok=True)
+        # Convert the 1D data to a 2D numpy array
+        occupancy_grid_2d = np.array(msg.data).reshape((height, width)).astype(np.float64)
+        image = Image.fromarray(occupancy_grid_2d)
+        image = image.convert('RGB')
+        image_path = os.path.join(save_dir, str(time.time())+".jpg")
+        image.save(image_path)
 
-    # Print the 2D array (for demonstration purposes)
-    rospy.loginfo("Occupancy Grid 2D Array:")
-    rospy.loginfo(occupancy_grid_2d)
 
-def listener():
-    # Initialize the ROS node
-    rospy.init_node('occupancy_grid_listener', anonymous=True)
+def main(args=None):
+    rclpy.init(args=args)
+    map_subscriber = MapSubscriber()
+    rclpy.spin(map_subscriber)
+    map_subscriber.destroy_node()
+    rclpy.shutdown()
 
-    # Subscribe to the occupancy grid topic
-    rospy.Subscriber('/map', OccupancyGrid, occupancy_grid_callback)
-
-    # Keep the node running
-    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        listener()
-    except rospy.ROSInterruptException:
-        pass
+    main()
